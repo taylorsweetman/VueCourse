16137d49b69d4eb3a63aff2457b6d422eeb82fc6
Taylor Sweetman <taylor.sweetman@gmail.com>
2020-11-12 13:50:40 -0500

    completed basics-03

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      userName: '',
+      confirmedName: ''
     };
   },
+  methods: {
+    confirmName() {
+      this.confirmedName = this.userName;
+    },
+    increment(num) {
+      this.counter = this.counter + num;
+    },
+    decrement(num) {
+      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.userName = event.target.value + ' ' + lastName;
+    }, 
+    submitForm() {
+      alert('Form Submitted!');
+    }
+  }
 });
 
 app.mount('#events');
