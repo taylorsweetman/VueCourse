d547840c7b33dbabb662f79da3e98b78f6473164
Taylor Sweetman <taylor.sweetman@gmail.com>
2020-11-13 15:59:26 -0500

    started basics 10

---
+++ basics-10-styling-starting-setup/app.js
@@ -0,0 +1,22 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      boxASel: false,
+      boxBSel: false,
+      boxCSel: false,
+    };
+  },
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASel = !this.boxASel;
+      } else if (box === "B") {
+        this.boxBSel = !this.boxBSel;
+      } else if (box === "C") {
+        this.boxCSel = !this.boxCSel;
+      }
+    },
+  },
+});
+
+app.mount("#styling");
