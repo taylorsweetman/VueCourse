db0f3598bc74146454c74f5d0f96aa7b7a126858
Taylor Sweetman <taylor.sweetman@gmail.com>
2020-11-23 16:09:56 -0500

    started working with components in node env

---
+++ Module 7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+      <friend-contact></friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "09876 543 221",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+  computed: {},
+};
+</script>
